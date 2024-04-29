61fb7aa1a420c9ec2922d33cccdb0a07cb1bb91f
ithalo gob <ithalolgob123@gmail.com>
2024-04-29 19:55:20 -0300

    proejto conta de banco java basico

---
+++ TIPOS-VARIAVEIS/src/VARIAVEIS.java
@@ -0,0 +1,18 @@
+public class VARIAVEIS {
+    public static void main(String[] args) throws Exception {
+        
+        int a, b;
+        String nomeUm = "Ithalo";
+        String nomeDois = new String ("Ithalo"); 
+        a = 6;
+        b = 6;
+
+        //String resultado = a==b ? "Verdadeiro" : "falso";
+        //&& = E, || = OU
+        
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+
+    }
+}
